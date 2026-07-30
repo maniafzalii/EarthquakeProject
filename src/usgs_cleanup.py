2a77eaf6f0de07c03c067323d75f219a7c651738
Manii <manieskandari05@gmail.com>
2026-07-30 18:56:36 +0330

    db setup and usgs_cleanup bugs fixed

--- src/usgs_cleanup.py
+++ src/usgs_cleanup.py
@@ -8,13 +8,14 @@ def cleanup_usgs_data():
 
     raw_df = pd.read_csv('../data/raw/JAPAN_USGS.csv')
     renamed_df = raw_df.rename(columns={'mag': 'magnitude', 'id': 'e_id'})
-    columns = ['time', 'latitude', 'longitude', 'depth', 'magnitude', 'place', 'source']
-    for column in columns:
+    renamed_df['time'] = pd.to_datetime(renamed_df['time']).dt.strftime("%Y-%m-%d %H:%M:%S")
+    target_columns = ['time', 'latitude', 'longitude', 'depth', 'magnitude', 'place', 'source']
+    for column in target_columns:
         if column not in renamed_df.columns and column == 'source':
             renamed_df[column] = "usgs"
         elif column not in renamed_df.columns:
             renamed_df[column] = None
-    final_df = renamed_df[columns]
+    final_df = renamed_df[target_columns]
     final_df.to_csv('../data/clean/cleaned_usgs.csv', index=False)
     return True
 
