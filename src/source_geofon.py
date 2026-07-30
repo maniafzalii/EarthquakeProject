c90dccdd742be0007dceae44355fa73dc5341126
Shayan_Naseri <shayan.naseri.aghdam@gmail.com>
2026-07-30 13:56:23 +0330

    we can scrap many pages

--- src/source_geofon.py
+++ src/source_geofon.py
@@ -27,17 +27,15 @@ longitudes = []
 latitudes = []
 magnitudes = []
 
-response = requests.get(url, params=params)
-soup = BeautifulSoup(response.content, "html.parser")
 
-def calculate_magnitude():
+def calculate_magnitude(soup):
     magnitudes_1 = soup.find_all('span', class_="magbox")
     for magnitude in magnitudes_1:
         mag = float(magnitude.text.strip())
         magnitudes.append(mag)
 
 
-def calculate_epicenter():
+def calculate_epicenter(soup):
     epicenters = soup.find_all('div', class_="col-xs-12")
     for epicenter in epicenters:
         if epicenter.get("title") != None:
@@ -46,13 +44,13 @@ def calculate_epicenter():
             latitudes.append(float(data_1.split(', ')[1].replace("°N", "")))
 
 
-def calculate_places():
+def calculate_places(soup):
     places_1 = soup.find_all('strong')
     for place in places_1:
         places.append(place.text)
 
 
-def calculate_depth():
+def calculate_depth(soup):
     depth_1 = soup.find_all('span', class_="pull-right")
     for data_1 in depth_1:
         if re.match(r'^\d', data_1.text.strip()):
@@ -60,7 +58,7 @@ def calculate_depth():
             depths.append(depth)
 
 
-def calculate_time():
+def calculate_time(soup):
     time_1 = soup.find_all('div', class_="col-xs-12")
     for time in time_1:
         if re.match(r"^\d", time.contents[0]):
@@ -76,19 +74,38 @@ data = {
     'longitude': longitudes,
     'depth': depths,
     'magnitude': magnitudes,
-    'source': "GEOFON",
-    'place': places
+    'place': places,
+    'source': "GEOFON"
+
 }
 
-def main_geofon():
-    calculate_time()
-    calculate_magnitude()
-    calculate_epicenter()
-    calculate_places()
-    calculate_depth()
+page_checking = 40
+page = 1
+while True:
+
+    params["page"] = page
+    response = requests.get(url, params=params)
+    soupp = BeautifulSoup(response.content, "html.parser")
+
+    print(response.status_code)
 
 
-main_geofon()
+    def main_geofon():
+        calculate_time(soupp)
+        calculate_magnitude(soupp)
+        calculate_epicenter(soupp)
+        calculate_places(soupp)
+        calculate_depth(soupp)
+
+
+    main_geofon()
+
+    if len(magnitudes) < page_checking:
+        break
+    else:
+        params["page"] = page
+        page_checking += 40
+        page += 1
 
 df = pandas.DataFrame(data)
 
@@ -96,5 +113,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 output_dir = BASE_DIR / "data" / "raw"
 output_dir.mkdir(parents=True, exist_ok=True)
+output_clean_dir = BASE_DIR / "data" / "clean"
+output_clean_dir.mkdir(parents=True, exist_ok=True)
 
 df.to_csv(output_dir / "JAPAN_GEOFON.csv", index=False)
+df.to_csv(output_clean_dir / "cleaned_GEOFON.csv", index=False)
