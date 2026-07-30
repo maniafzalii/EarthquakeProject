b1f6ab2b72a8e95f081792f1e5f600cd7937e17f
shiva <shiiva.shrn@gmail.com>
2026-07-30 10:26:20 +0330

    source_emsc.py modifies and JAPAN_EMSC.csv created

--- src/source_emsc.py
+++ src/source_emsc.py
@@ -1,5 +1,7 @@
 import time 
 import datetime
+import pandas as pd
+from pathlib import Path
 from dateutil.relativedelta import relativedelta
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
@@ -22,8 +24,8 @@ def get_min_time():
 
 def scrape_emsc():
     all_earthquake_information=[]
-    #options=Options()
-    #options.add_argument("--headless")
+    options=Options()
+    options.add_argument("--headless")
     driver=webdriver.Firefox()
     driver.get("https://www.emsc.eu/Earthquake_information/")
     driver.maximize_window()
@@ -34,7 +36,7 @@ def scrape_emsc():
         cookie_selector.click()
         time.sleep(2)
     except Exception as ex:
-        print("Exception Raised Cookie Not Found ",ex)    
+        print("Exception:Cookie Not Found! ",ex)    
     previous_date=get_min_time() 
     current_date=get_max_time()
     wait = WebDriverWait(driver, 5)
@@ -45,14 +47,14 @@ def scrape_emsc():
         driver.execute_script("arguments[0].value = arguments[1];", date_min_box, previous_date)
         driver.execute_script("arguments[0].dispatchEvent(new Event('change'));", date_min_box)
     except Exception as ex:
-        print("Exception in Start Date Entrance ", ex)
+        print("Exception:Start Date Entrance Failed! ", ex)
     #set max date    
     try: 
         date_max_box = wait.until(EC.presence_of_element_located((By.ID, 'datemax')))
         driver.execute_script("arguments[0].value = arguments[1];", date_max_box, current_date)
         driver.execute_script("arguments[0].dispatchEvent(new Event('change'));", date_max_box)
     except Exception as ex:
-        print("Exception in End Date Entrance ", ex)   
+        print("Exception!End Date Entrance Failed! ", ex)   
     #set region    
     try:
         search_box=driver.find_element(By.ID,'reg')
@@ -66,7 +68,7 @@ def scrape_emsc():
         send_check.click()
         time.sleep(1)
     except Exception as ex:
-        print("Exception Search Process Failed  ",ex)    
+        print("Exception:Search Process Failed!  ",ex)    
 
     #click search button    
     try:    
@@ -75,46 +77,73 @@ def scrape_emsc():
         submit_button.click()
         time.sleep(2)
     except Exception as ex:
-        print("Exception Search  Button Not Found ",ex)  
+        print("Exception:Search Button Not Found! ",ex)  
 
     #find search result
-    try:
-        search_content=driver.find_element(By.CSS_SELECTOR,'div[class="htab"]')
-        table_content=driver.find_element(By.CSS_SELECTOR,'eqs.table-scroll')
+    page_number=1
+    all_pages_extracted=False
+    while not(all_pages_extracted):
+        print(f"Scraping Page  {page_number}")
+        try:
+            search_content=driver.find_element(By.CSS_SELECTOR,'div[class="htab"]')
+            table_content=search_content.find_element(By.CLASS_NAME,'eqs.table-scroll')
 
-        #scroll page
-        last_height=driver.execute_script("return document.body.scrollHeight")
-        while True:
-            driver.execute_script('window.scrollTo(0,document.body.scrollHeight)')
-            time.sleep(5)
-            new_height=driver.execute_script("return document.body.scrollHeight")
-            if new_height==last_height:
-                break
-            last_height=new_height
+            #scroll page
+            last_height=driver.execute_script("return document.body.scrollHeight")
+            while True:
+                driver.execute_script('window.scrollTo(0,document.body.scrollHeight)')
+                time.sleep(5)
+                new_height=driver.execute_script("return document.body.scrollHeight")
+                if new_height==last_height:
+                   break
+                last_height=new_height
 
-        #find data of each row (every earthquake)    
-        earthquake_search_data=table_content.find_elements(By.CSS_SELECTOR,'tr')
-        for data in earthquake_search_data:
-            #fetch date
+            #find data of each row (every earthquake)    
+            earthquake_search_data=table_content.find_elements(By.CSS_SELECTOR,'tr')
+            for data in earthquake_search_data:
+                #fetch date
+                try:
+                    earthquake_date_full=data.find_element(By.CSS_SELECTOR,'td.tbdat>a')
+                    earthquake_date=earthquake_date_full.text.split('\n')[0].strip()
+                    earthquake_latitude=data.find_element(By.CLASS_NAME,'tblat').text
+                    earthquake_longitude=data.find_element(By.CLASS_NAME,'tblon').text
+                    earthquake_depth=data.find_element(By.CLASS_NAME,'tbdep').text
+                    earthquake_magnitude=data.find_element(By.CLASS_NAME,'tbmag').text
+                    earthquake_region=data.find_element(By.CLASS_NAME,'tbreg').text
+ 
+                    #save information of each row in a dictionary and append to list of all earthquakes
+                    earthquake_info={'date ':earthquake_date,'latitude ':earthquake_latitude,'longtitude ':earthquake_longitude,'depth ':earthquake_depth,
+                             'magnitude ':earthquake_magnitude,'region ':earthquake_region} 
+                    all_earthquake_information.append(earthquake_info)   
+                except Exception as es:
+                    print("Exception:Earthquake Data Not Found!  ",es)  
+                    continue     
+            
             try:
-                earthquake_date=data.find_element(By.CSS_SELECTOR,'a[href*="earthquake.php"]').text.split("\n")[0].strip()  
-                earthquake_latitude=data.find_element(By.CLASS_NAME,'tblat').text
-                earthquake_longitude=data.find_element(By.CLASS_NAME,'tblon').text
-                earthquake_depth=data.find_element(By.CLASS_NAME,'tbdep').text
-                earthquake_magnitude=data.find_element(By.CLASS_NAME,'tbmag').text
-                earthquake_region=data.find_element(By.CLASS_NAME,'tbreg').text
-
-                #save information of each row in a dictionary and append to list of all earthquakes
-                earthquake_info={'date':earthquake_date,'latitude':earthquake_latitude,'longtitude':earthquake_longitude,'depth':earthquake_depth,
-                             'magnitude':earthquake_magnitude,'region':earthquake_region} 
-                all_earthquake_information.append(earthquake_info)   
-            except Exception as es:
-                print("Exception longtitude not found ",es)  
-       
-    except Exception as ex:
-        print("Exception Fetch Content Unseccesful ",ex)    
-          
-
+                next_page = driver.find_element(By.XPATH, "//div[@class='page-cont']/div[contains(@class, 'spes') and text()='›']")
+                
+                if "dis" in next_page.get_attribute("class"):
+                    #all pages extracted
+                    all_pages_extracted = True
+                else:
+                    next_page.click()
+                    page_number += 1
+                    time.sleep(3) 
+            except Exception as e:
+                print("Exception:Next Page Not Found! ", e)
+                all_pages_extracted = True
+                                                    
+                                                     
+        except Exception as ex:
+            print("Exception Fetch Content Unseccesful ",ex)  
+    erathquake_table=pd.DataFrame(all_earthquake_information)      
+    #get the path of csv file  
+    current_script_path = Path(__file__).resolve()  
+    project_root = current_script_path.parent.parent       
+    raw_folder = project_root / 'data' / 'raw'    
+    raw_folder.mkdir(parents=True, exist_ok=True) 
+    file_path = raw_folder / 'JAPAN_EMSC.csv' 
+    erathquake_table.to_csv(file_path,index=False)
     driver.quit()    
 
 scrape_emsc()
